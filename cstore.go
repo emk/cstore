0e7b083c8dc7d4ace4fe3ffaba566541188dd6ab
Eric Kidd <git@randomhacks.net>
2011-12-26 06:36:55 -0500

    PUT and GET blobs of content

--- cstore.go
+++ cstore.go
@@ -3,12 +3,81 @@ package cstore
 import (
 	"http"
 	"io"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
+	"sync"
 )
 
-func dummyHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Testing.\n")
+var (
+	url_regexp = regexp.MustCompile("^/([0-9a-f]*)$")
+)
+
+type handler struct {
+	content map[string][]byte
+	locker sync.Locker
+}
+
+// Safely store content in our hash table.
+func (h *handler) setContent(digest string, content []byte) {
+	h.locker.Lock()
+	defer h.locker.Unlock()
+	h.content[digest] = content
+}
+
+// Safely fetch content from our hash table.  Return nil if we don't have
+// any content for the specified digest.
+func (h *handler) getContent(digest string) []byte {
+	h.locker.Lock()
+	defer h.locker.Unlock()
+	return h.content[digest]
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Println("Handling", req.Method, req.URL.Path)
+	match := url_regexp.FindStringSubmatch(req.URL.Path)
+	if match == nil {
+		w.WriteHeader(http.StatusForbidden)
+		io.WriteString(w, "Invalid resource path\n")
+		return
+	}
+	digest := strings.ToLower(match[1])
+	if len(digest) != 64 {
+		w.WriteHeader(http.StatusForbidden)
+		io.WriteString(w, "Invalid resource path\n")
+		return		
+	}
+
+	switch req.Method {
+	case "GET":
+		content := h.getContent(digest)
+		if content == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if _, err := w.Write(content); err != nil {
+			// TODO: Log write failure.
+			return
+		}
+	case "PUT":
+		content, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			io.WriteString(w, "Could not read payload\n")
+			return
+		}
+		// TODO: Check digest.
+		h.setContent(digest, content)
+		w.WriteHeader(http.StatusCreated)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
 
 func NewHandler() http.Handler {
-	return http.HandlerFunc(dummyHandler)
+	handler := new(handler)
+	handler.locker = new(sync.Mutex)
+	handler.content = make(map[string][]byte)
+	return handler
 }
--- cstore_test.go
+++ cstore_test.go
@@ -1,11 +1,22 @@
 package cstore
 
 import (
-	"testing"
+	"crypto"
+	"encoding/hex"
 	"http"
 	"http/httptest"
 	"io/ioutil"
+	"strings"
+	"testing"
 )
+
+func digest(data string) string {
+	hash := crypto.SHA256.New()
+	if _, err := hash.Write([]byte(data)); err != nil {
+		panic("Writing to a hash should never fail");
+	}
+	return hex.EncodeToString(hash.Sum())
+}
 
 func assertStringsEqual(t *testing.T, expected, got string) {
 	if expected != got {
@@ -22,7 +33,7 @@ func assertResponseBody(t *testing.T, expected string, r *http.Response) {
 	assertStringsEqual(t, expected, string(body))
 }
 
-func assertHttpGet(t *testing.T, client http.Client, expected, url string) {
+func assertHttpGet(t *testing.T, client *http.Client, expected, url string) {
 	r, err := client.Get(url)
 	if err != nil {
 		t.Errorf("Can't GET %s: %v", url, err)
@@ -37,7 +48,26 @@ func TestServer(t *testing.T) {
 	server := httptest.NewServer(NewHandler())
 	defer server.Close()
 
-	// Make sure it returns something when called.
+        // Define our data and where to put it.
+        data := "Testing!"
+        hash := digest(data)
+        url := server.URL + "/" + hash
+
+	// PUT our data to the server.
 	client := new(http.Client)
-	assertHttpGet(t, client, "Testing.\n", server.URL)
+	req, err := http.NewRequest("PUT", url, strings.NewReader(data))
+	if err != nil {
+		t.Fatalf("Can't build HTTP request: %s", err)
+	}
+	resp, err := client.Do(req)
+        if err != nil {
+		t.Fatalf("Can't PUT data: %s", err)
+	}
+        defer resp.Body.Close()
+        if resp.StatusCode != http.StatusCreated {
+		t.Fatalf("Unexpected HTTP response: %s", resp.Status)
+	}
+
+	// Make sure it returns something when called.
+	assertHttpGet(t, client, data, url)
 }
